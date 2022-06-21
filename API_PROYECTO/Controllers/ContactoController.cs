75b7a2663b66051909a34a251401767fdcb60da2
sacerdosiacobvs <adrian.solano.g@gmail.com>
2022-06-20 19:59:18 -0600

    Agregar archivos de proyecto.

---
+++ API_PROYECTO/Controllers/ContactoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using API_PROYECTO.Models;
+using System.Collections;
+using Nancy.Json;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_PROYECTO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController : ControllerBase
+    {
+        [Route("Obtener_Contactos")]
+        //[HttpGet("{id}")]
+        //public string Obtener_Contactos(int id)
+        public string Obtener_Contactos()
+        {
+            Contacto contacto = new Contacto();
+
+            DataTable dt = contacto.Obtener_Contactos_Tabla();
+
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, object> row;
+            foreach (DataRow dr in dt.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+            return serializer.Serialize(rows).ToUpper();
+        }
+
+    }
+}
---
+++ API_PROYECTO/Models/Contacto.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace API_PROYECTO.Models
+{
+    public class Contacto
+    {
+        public int id_contacto { get; set; }
+        public int id_persona { get; set; }
+        public int correo { get; set; }
+        public int puesto { get; set; }
+
+        string connectionString = "Server=localhost;Database=casos_legales;Uid=root;Pwd=123456;convert zero datetime=True;";
+
+        public DataTable Obtener_Contactos_Tabla()
+        {
+            DataTable dt = new DataTable();
+            MySqlConnection con = new MySqlConnection(connectionString);
+            con.Open();
+            MySqlCommand sql = new MySqlCommand("SELECT * FROM contactos JOIN personas WHERE contactos.ID_PERSONA = personas.ID_PERSONA;", con);
+
+            dt.Load(sql.ExecuteReader());
+            con.Close();
+            return dt;
+        }
+
+    }
+}
